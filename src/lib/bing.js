2c010fe452055d9e1553a620c561ec706519fb07
waliurrahman <mwaliurrahman@gmail.com>
2023-03-12 21:28:39 +0600

    New 6

--- src/lib/bing.js
+++ src/lib/bing.js
@@ -8,7 +8,7 @@ export async function getSearchResults(query) {
     }
   );
   const data = await response.json();
-  console.log(data)
+  // console.log(data)
   // return data;
   return data.webPages.value;
 }
